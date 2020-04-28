976cedf61b4bc43221ae4fae5a23d54522058e5b
sidorenko <sidorenko4er@gmail.com>
2020-04-28 10:37:01 +0300

    added Providers seeders

---
+++ database/seeds/VoyagerProvidersPermissionsSeeder.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Marqant\MarqantPayVoyager\Database\Seeds;
+
+use TCG\Voyager\Models\Role;
+use Illuminate\Database\Seeder;
+use TCG\Voyager\Models\Permission;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * Class VoyagerProvidersPermissionsSeeder
+ *
+ * @package Marqant\MarqantPayVoyager\Seeds
+ */
+class VoyagerProvidersPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     *
+     * @throws \Throwable
+     */
+    public function run()
+    {
+        DB::transaction(function () {
+            $browse = Permission::updateOrCreate([
+                'key'        => 'browse_providers',
+                'table_name' => 'providers',
+            ]);
+
+            $read = Permission::updateOrCreate([
+                'key'        => 'read_providers',
+                'table_name' => 'providers',
+            ]);
+
+            $edit = Permission::updateOrCreate([
+                'key'        => 'edit_providers',
+                'table_name' => 'providers',
+            ]);
+
+            $add = Permission::updateOrCreate([
+                'key'        => 'add_providers',
+                'table_name' => 'providers',
+            ]);
+
+            $delete = Permission::updateOrCreate([
+                'key'        => 'delete_providers',
+                'table_name' => 'providers',
+            ]);
+
+            // get admin role
+            $role = Role::where('name', 'admin')
+                ->first();
+            /** @var Role $role attach permissions to admin */
+            $role->permissions()
+                ->attach($browse);
+            $role->permissions()
+                ->attach($read);
+            $role->permissions()
+                ->attach($edit);
+            $role->permissions()
+                ->attach($add);
+            $role->permissions()
+                ->attach($delete);
+        });
+    }
+}
