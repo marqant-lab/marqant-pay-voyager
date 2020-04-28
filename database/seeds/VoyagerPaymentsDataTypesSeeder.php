52fedebf1250e4b66156b128ee24bfe4ecdd9f43
sidorenko <sidorenko4er@gmail.com>
2020-04-28 10:37:35 +0300

    added Payments seeders

---
+++ database/seeds/VoyagerPaymentsDataTypesSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Marqant\MarqantPayVoyager\Database\Seeds;
+
+use Illuminate\Database\Seeder;
+use TCG\Voyager\Models\DataType;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * Class VoyagerPaymentsDataTypesSeeder
+ *
+ * @package Marqant\MarqantPayVoyager\Seeds
+ */
+class VoyagerPaymentsDataTypesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     *
+     * @throws \Throwable
+     */
+    public function run()
+    {
+        DB::transaction(function () {
+            DataType::updateOrCreate([
+                'slug' => 'payments',
+            ], [
+                'name' => 'payments',
+                'display_name_singular' => 'Payment',
+                'display_name_plural' => 'Payments',
+                'icon' => 'voyager-wallet',
+                'model_name' => config('marqant-pay.payment_model'),
+                'policy_name' => null,
+                'controller' => '',
+                'description' => '',
+                'generate_permissions' => 1,
+                'server_side' => 0,
+                'details' => null,
+            ]);
+        });
+    }
+}
